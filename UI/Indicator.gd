38b4826783b35ad4429a4fe389ab0f6bb2a43b2e
Vidaliar <97802043+Vidaliar@users.noreply.github.com>
2023-11-06 10:35:23 -0500

    Project 3

--- UI/Indicator.gd
+++ UI/Indicator.gd
@@ -5,13 +5,14 @@ var mod = 0
 var scale_target = Vector2(0.75,0.75)
 var sca = Vector2(0.5,0.5)
 
-var tween
+var tween = null
 
 func _ready():
-	breathe()
+	tween_indicator()
+	$Highlight.scale = sca
+	$Highlight.modulate.a = mod
 
-
-func breathe():
+func tween_indicator():
 	mod = 0.0 if mod == modulate_target else modulate_target
 	sca = Vector2(0.5,0.5) if sca == scale_target else scale_target
 	if tween:
@@ -20,4 +21,4 @@ func breathe():
 	tween.tween_property($Highlight, "scale", sca, 1.0)
 	tween.tween_property($Highlight, "modulate:a", mod, 1.0)
 	tween.set_parallel(false)
-	tween.tween_callback(breathe)
+	tween.tween_callback(tween_indicator)
--- Wall/Wall.gd
+++ Wall/Wall.gd
@@ -12,7 +12,8 @@ func _physics_process(_delta):
 		$ColorRect.color.v += decay
 
 func hit(_ball):
-	$ColorRect.color = Color8(201,42,42,255)
-	var wall_sound = get_node_or_null("/root/Game/Wall_Sound")
-	if wall_sound != null:
-		wall_sound.play()
+	$ColorRect.color = Color8(201,42,42)
+	var wall_audio = get_node_or_null("/root/Game/Wall_Audio")
+	if wall_audio != null:
+		wall_audio.play()
+	
